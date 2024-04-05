3074b68c1035c8f2ecc2c5b46b23ce6a4c3d11a5
charankumarl <charankumar1224@gmail.com>
2024-04-05 10:29:55 +0530

    temp branch commit 2nd day

--- GitProjects/GitProjects/src/main/java/com/p1/abc.java
+++ GitProjects/GitProjects/src/main/java/com/p1/abc.java
@@ -2,11 +2,11 @@ package com.p1;
 
 public class abc {
     public static void main(String[] args) {
-        System.out.println("suhfusdfjsdhfjsdhbfv");
-
-        System.out.println("abcdefghijk");
 
 
         System.out.println("new branch push merege");
+
+        System.out.println("new branch 2 times push merege");
+
     }
 }
