68e98b4f689425042cbe1046fcf672a52bae416c
charankumarl <charankumar1224@gmail.com>
2024-04-05 11:07:51 +0530

    new direc commit

--- GitProjects/GitProjects/src/main/java/com/p1/abc.java
+++ GitProjects/GitProjects/src/main/java/com/p1/abc.java
@@ -4,9 +4,9 @@ public class abc {
     public static void main(String[] args) {
 
 
-        System.out.println("new branch push merege");
+        System.out.println("charan");
 
-        System.out.println("new branch 2 times push merege");
+        System.out.println("kumar");
 
     }
 }
