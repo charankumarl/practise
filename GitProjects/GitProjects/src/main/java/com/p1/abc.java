e34851d326062052176865af03a1b114efd32635
charankumarl <charankumar1224@gmail.com>
2024-04-05 10:08:09 +0530

    new day 1st commit~

--- GitProjects/GitProjects/src/main/java/com/p1/abc.java
+++ GitProjects/GitProjects/src/main/java/com/p1/abc.java
@@ -4,13 +4,8 @@ public class abc {
     public static void main(String[] args) {
         System.out.println("suhfusdfjsdhfjsdhbfv");
 
-        System.out.println("suhfusdfjsdhfjsdhbfv");
+        System.out.println("abcdefghijk");
 
-
-        System.out.println("suhfusdfjsdhfjsdhbfv");
-        System.out.println("suhfusdfjsdhfjsdhbfv");
-
-        System.out.println("suhfusdfjsdhfjsdhbfv");
 
 
     }
